3b259fb9a5963f3d2e45b60f1d2fea38ef3cbfec
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-12-25 00:13:35 +0100

    Hands text

--- Project Wilcholas/Assets/Scripts/PlayerAttack.cs
+++ Project Wilcholas/Assets/Scripts/PlayerAttack.cs
@@ -6,11 +6,20 @@ public class PlayerAttack : MonoBehaviour {
 
 	public GameObject projectile;
 	public Transform shootPoint;
-
-	[Header("Variables")]
 	[SerializeField] private float shootForce = 25.0f;
 
-	public void Shoot (bool value) {
+	private void Update () {
+		ShootController();
+	}
+
+	private void ShootController () {
+		if(Input.GetButtonDown("Fire1"))
+		{
+			BasicAttack();
+		}
+	}
+
+	private void BasicAttack () {
 		//Spawn projectile and add force
 		GameObject go = Instantiate(projectile, shootPoint.position, shootPoint.rotation);
 		go.GetComponent<Rigidbody>().AddForce((transform.forward * shootForce) * 100.0f);
