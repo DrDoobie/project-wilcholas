452511fa46c0d12d3d023d6d5d3004cde112d045
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-12-24 22:47:40 +0100

    Textures folder

---
+++ Project Wilcholas/Assets/Scripts/Billboard.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class Billboard : MonoBehaviour
+{
+    private Camera mainCam;
+ 
+    private void Start () {
+        mainCam = Camera.main;
+    }
+
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + mainCam.transform.rotation * Vector3.forward,
+            mainCam.transform.rotation * Vector3.up);
+    }
+
+	//Code by Neil Carter
+}
