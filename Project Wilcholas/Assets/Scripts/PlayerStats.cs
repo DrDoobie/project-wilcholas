6ac14ffe67fda86cf579a05fd87dcffd22539ba1
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-12-24 22:16:56 +0100

    Stats working

---
+++ Project Wilcholas/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour {
+
+	[SerializeField] private float health, stamina, mana, statLimit;
+	[SerializeField] private Slider healthBar, staminaBar, manaBar;
+	
+	private void Start () {
+		SetValues();
+	}
+
+	private void Update () {
+		StatController();
+	}
+
+	private void SetValues () {
+		healthBar.maxValue = statLimit;
+		staminaBar.maxValue = statLimit;
+		manaBar.maxValue = statLimit;
+	}
+
+	private void StatController () {
+		healthBar.value = health;
+		staminaBar.value = stamina;
+		manaBar.value = mana;
+	}
+}
