883bb17b4c523743ac298a3efae950ec9fb8c6e4
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-12-24 23:02:42 +0100

    Imported textures

---
+++ Project Wilcholas/Assets/Scripts/AIMotor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMotor : MonoBehaviour {
+
+	public Transform target;
+	private NavMeshAgent agent;
+
+	private void Start () {
+		SetValues();
+	}
+
+	private void SetValues () {
+		agent = this.gameObject.GetComponent<NavMeshAgent>();
+	}
+}
