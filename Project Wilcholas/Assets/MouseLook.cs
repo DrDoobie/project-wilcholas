552817cfd0726898377bf0ef1585736cde17d891
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-12-24 21:55:01 +0100

    Player setup

---
+++ Project Wilcholas/Assets/MouseLook.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public bool invertedMouseY = false;
+    [SerializeField] private float mouseSens = 7.5f;
+    private float xClamp = 0.0f;
+
+    private void Awake() {
+        CursorLock();
+    }
+
+    private void Update() {
+        LookController();
+    }
+
+    private void CursorLock() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LookController()
+    {
+        //Record mouse x and y movement
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSens;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSens;
+
+        //Limit camera rotation
+        xClamp += mouseY;
+
+        if (xClamp > 90.0f)
+        {
+            xClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXRotation(270.0f);
+
+        } else if (xClamp < -90.0f) {
+            xClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXRotation(90.0f);
+        }
+
+        //Rotate camera on Y axis
+        if (!invertedMouseY)
+        {
+            transform.Rotate(Vector3.left * mouseY);
+
+        } else {
+            transform.Rotate(-Vector3.left * mouseY);
+        }
+
+        //Rotate player around Y axis (look left to right)
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXRotation(float value) {
+        Vector3 eulerRot = transform.eulerAngles;
+        eulerRot.x = value;
+        transform.eulerAngles = eulerRot;
+    }
+}
---
+++ Project Wilcholas/Assets/PlayerMotor.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private bool isJumping;
+    private CharacterController controller;
+    [SerializeField] private float walkSpeed = 5.0f, sprintSpeed = 7.5f, jumpMultiplier = 10.0f, slopeForce = 2.0f;
+    [SerializeField] private AnimationCurve jumpCurve;
+    private float movementSpeed;
+
+    private void Awake() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        MovementController();
+    }
+
+    private void MovementController() {
+        if (IsSprinting())
+        {
+            movementSpeed = sprintSpeed;
+
+        } else {
+            movementSpeed = walkSpeed;
+        }
+
+        //Record horizontal and vertical input
+        float vertInput = Input.GetAxis("Vertical");
+        float horizInput = Input.GetAxis("Horizontal");
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 sideMovement = transform.right * horizInput;
+
+        //Move player with horizontal and vertical input
+        controller.SimpleMove(Vector3.ClampMagnitude(forwardMovement + sideMovement, 1.0f) * movementSpeed);
+
+        if ((vertInput != 0) || (horizInput != 0) && OnSlope())
+        {
+            controller.Move((Vector3.down * (controller.height / 2) * slopeForce) * Time.deltaTime);
+        }
+
+        Jump();
+    }
+
+    private bool IsSprinting() {
+        if (Input.GetButton("Sprint"))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool OnSlope() {
+        if (isJumping)
+        {
+            return false;
+        }
+
+        //Cast a ray to determine if player is on a slope or not
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, (controller.height / 2) * 1.5f))
+        {
+            if (hit.normal != Vector3.up)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void Jump() {
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent() {
+        controller.slopeLimit = 90.0f;
+        float airTime = 0.0f;
+
+        do
+        {
+            //Set jump force
+            float jumpForce = jumpCurve.Evaluate(airTime);
+            //Jump happens here
+            controller.Move((Vector3.up * jumpForce * jumpMultiplier) * Time.deltaTime);
+            airTime += Time.deltaTime;
+
+            yield return null;
+
+            //Player will stop jumping if collides into something above
+        } while ((!controller.isGrounded) && (controller.collisionFlags != CollisionFlags.Above));
+
+        controller.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+}
