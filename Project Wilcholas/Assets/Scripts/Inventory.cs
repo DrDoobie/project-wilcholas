12ed0da934982fc97553f0c27c63361297853310
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-12-25 23:55:32 -0500

    Inventory

---
+++ Project Wilcholas/Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	[SerializeField] private Transform inventory;
+	[SerializeField] private List<GameObject> slots;
+
+	private void Start () {
+		SlotSetup();
+	}
+
+	private void SlotSetup () {
+		for(int i = 0; i < inventory.childCount; i++)
+		{
+			slots.Add(inventory.GetChild(i).gameObject);
+		}
+	}
+}
