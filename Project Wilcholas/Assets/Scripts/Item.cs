cdea2d26a2966c11522767a304ce5c2a50bf0cc6
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-01-03 10:08:45 -0500

    Consumable items

---
+++ Project Wilcholas/Assets/Scripts/Item.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	[SerializeField] private bool inventoryItem;
+	[Header("Values")]
+	[SerializeField] private bool health;
+	[SerializeField] private bool stamina;
+	[SerializeField] private bool mana;
+	[SerializeField] private float value;
+	private GameObject player;
+
+	private void Awake () {
+		player = GameObject.FindWithTag("Player");
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		if((!inventoryItem) && (other.tag == "Player"))
+		{
+			UseItem();
+		}
+	}
+
+	private void UseItem () {
+		//Dirt code here, must clean later
+		if(health)
+		{
+			player.GetComponent<PlayerStats>().health += value;
+
+		} else if(stamina) {
+			player.GetComponent<PlayerStats>().stamina += value;
+
+		} else if(mana) {
+			player.GetComponent<PlayerStats>().mana += value;
+		}
+
+		Destroy(this.gameObject);
+	}
+}
