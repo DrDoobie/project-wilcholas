68942d3298d4e02f0233fef15ab0159250d7aa32
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-01-15 20:29:42 +0100

    Self destruct script

---
+++ Project Wilcholas/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+	[SerializeField] private float destructTime;
+
+	private void Awake () {
+		Destroy(this.gameObject, destructTime);
+	}
+}
