954bd17bc8267b53b2b06e1779f7c6de174e5142
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-01-18 19:27:26 +0100

    Dungeon generation

--- Project Wilcholas/Assets/Scripts/Dungeons/TileSpawner.cs
+++ Project Wilcholas/Assets/Scripts/Dungeons/TileSpawner.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 
 public class TileSpawner : MonoBehaviour {
 
-	[SerializeField] private int direction;
-	[SerializeField] private GameObject[] tiles, spawns;
+	//1- Requires +x door, 2- Requires -z door, 3- Requires -x door, 4- Requires +z
+	[SerializeField] private int openDir;
+	private bool spawned = false;
+	private TileTemplates templates;
 
-	private void Awake () {
-		SpawnTiles();
+	private void Start () {
+		templates = GameObject.FindWithTag("TileTemplates").GetComponent<TileTemplates>();
+			Invoke("SpawnTiles", 0.1f);
 	}
 
 	private void SpawnTiles () {
-		//Clean here later
-		int spawn = 0;
-		int tile = 0;
-
-		foreach(var GameObject in spawns)
+		if(!spawned)
 		{
-			if(direction == 0)
+			int val = 0;
+
+			if(openDir == 1)
 			{
-				Instantiate(tiles[tile], spawns[spawn].transform.position, spawns[spawn].transform.rotation);
-					tile++;
+				val = Random.Range(0, templates.posX.Length);
+					Instantiate(templates.posX[val], transform.position, Quaternion.identity);
+
+			} else if(openDir == 2) {
+				val = Random.Range(0, templates.negZ.Length);
+					Instantiate(templates.posX[val], transform.position, Quaternion.identity);
+
+			} else if(openDir == 3) {
+				val = Random.Range(0, templates.negX.Length);
+					Instantiate(templates.posX[val], transform.position, Quaternion.identity);
+
+			} else if(openDir == 4) {
+				val = Random.Range(0, templates.posZ.Length);
+					Instantiate(templates.posX[val], transform.position, Quaternion.identity);
 			}
 
-			spawn++;
-		}		
+			spawned = true;
+		}
+	}
+
+	private void OnTriggerEnter (Collider other)
+	{
+		if((other.tag == "TileSpawn") && (other.GetComponent<TileSpawner>().spawned))
+		{
+			Destroy(gameObject);
+		}
 	}
 }
---
+++ Project Wilcholas/Assets/Scripts/Dungeons/TileTemplates.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTemplates : MonoBehaviour {
+
+	public GameObject[] posX, negX, posZ, negZ;
+}
