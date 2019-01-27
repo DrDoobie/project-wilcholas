a0fc300309e70f0f22b877bc51a0ed3e89045e39
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-01-27 21:24:12 +0100

    Enemies now have 33% chance of dropping loot

--- Project Wilcholas/Assets/Scripts/Misc/HealthController.cs
+++ Project Wilcholas/Assets/Scripts/Misc/HealthController.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour {
 
-	[SerializeField] private bool ai;
-	[SerializeField] private float health = 100.0f, value = 1.0f;
+	[SerializeField] private bool ai, dropsLoot;
 	[SerializeField] private Slider healthBar;
+	[SerializeField] private float health = 100.0f, value = 1.0f;
+	[SerializeField] private GameObject[] lootDrops;
 
 	private void Awake () {
 		healthBar.maxValue = health;
@@ -26,10 +27,25 @@ public class HealthController : MonoBehaviour {
 		}
 	}
 
+	private void DropLoot () {
+		int loot = Random.Range(0, lootDrops.Length);
+		Instantiate(lootDrops[loot], transform.position, Quaternion.identity);
+	}
+
 	private void Die () {
 		if(ai)
 		{
 			GameObject.FindWithTag("GameController").GetComponent<GameController>().currentAI--;
+		}
+
+		if(dropsLoot)
+		{
+			int val = Random.Range(0, 3);
+
+			if(val == 0)
+			{
+				DropLoot();
+			}
 		}
 
 		GameObject.FindWithTag("Player").GetComponent<PlayerExperience>().AddXp(value);
