f703eb73220be504244416d9485e58840e788707
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-01-31 21:48:02 +0100

    Merge

--- Project Wilcholas/Assets/Scripts/Misc/SceneChange.cs
+++ Project Wilcholas/Assets/Scripts/Misc/SceneChange.cs
@@ -7,17 +7,11 @@ public class SceneChange : MonoBehaviour {
 
 	[SerializeField] private int scene = 1;
 
-	private void ChangeScene () {
-		Vector3 zero = new Vector3(0, 0, 0);
-		GameObject.FindGameObjectWithTag("Player").transform.SetPositionAndRotation(zero, Quaternion.identity);
-		SceneManager.LoadScene(scene);
-	}
-
 	private void OnTriggerEnter (Collider other)
 	{
 		if(other.tag == "Player")
 		{
-			ChangeScene();
+			SceneManager.LoadScene(scene);
 		}
 	}
 }
