d0905484e3fb8627beaa22fcfa6b843d7edc0725
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-02-25 00:40:59 +0100

    Spells panel interactable

--- Project Wilcholas/Assets/Scripts/Player/Combat/PlayerSouls.cs
+++ Project Wilcholas/Assets/Scripts/Player/Combat/PlayerSouls.cs
@@ -7,13 +7,30 @@ public class PlayerSouls : MonoBehaviour {
 	[HideInInspector] public bool soulReady;
 	public GameObject spellsPanel;
 	private GameObject soulIndicator;
+	private bool open = false;
 
 	private void Start () {
 		soulIndicator = GameObject.FindWithTag("SoulIndicator");
 	}
 
 	private void Update () {
+		WindowController();
 		Indicator();
+	}
+
+	private void WindowController () {
+		if(Input.GetButtonDown("Spells"))
+		{
+			open = !open;
+		}
+
+		if(open)
+		{
+			spellsPanel.SetActive(true);
+			return;
+		}
+
+		spellsPanel.SetActive(false);
 	}
 	
 	private void Indicator () {
