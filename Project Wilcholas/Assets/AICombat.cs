1dfc69d79fcd17fb9925e426f33d822e14315b3c
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-03-10 15:20:45 +0100

    Started ai combat

---
+++ Project Wilcholas/Assets/AICombat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICombat : MonoBehaviour {
+
+	public GameObject prefab;
+	[SerializeField] private float force, shootSpeed;
+	private float ogShootSpeed;
+
+	private void Awake () {
+		ogShootSpeed = shootSpeed;
+	}
+
+	private void Update () {
+		CombatController();
+	}
+
+	private void CombatController () {
+		AIMotor motor = GetComponent<AIMotor>();
+
+		if(motor.agro)
+		{	
+			shootSpeed -= Time.deltaTime;
+
+			if(shootSpeed < 0.0f)
+			{
+				Shoot();
+				shootSpeed = ogShootSpeed;
+			}
+		}
+	}
+
+	private void Shoot () {
+		GameObject go = Instantiate(prefab, transform.position, transform.rotation, transform);
+		go.GetComponent<Rigidbody>().AddForce((transform.forward * force) * 100.0f);
+	}
+}
--- Project Wilcholas/Assets/Scripts/Player/Combat/Projectile.cs
+++ Project Wilcholas/Assets/Scripts/Player/Combat/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Projectile : MonoBehaviour {
 
+	public bool enemyProjectile;
 	public GameObject impactEffect, dmgPopup;
 	private float damage;
 	[SerializeField] private float minDamg = 7.0f, maxDamg = 12.0f;
@@ -27,12 +28,15 @@ public class Projectile : MonoBehaviour {
 
 	private void OnCollisionEnter (Collision col) {
 		//If collision with enemy is detected, deal damage
-		if(col.gameObject.tag == "Enemy")
+		if(col.gameObject.tag == "Enemy" && (!enemyProjectile))
 		{
 			col.gameObject.GetComponent<HealthController>().SubtractHealth(damage);
 			col.gameObject.GetComponent<AIMotor>().agro = true;
 			DamagePopup();
-		} 
+
+		} else if(col.gameObject.tag == "Player" && (enemyProjectile)) {
+			col.gameObject.GetComponent<PlayerStats>().health -= damage;
+		}
 
 		Instantiate(impactEffect, transform.position, transform.rotation);
 		Destroy(this.gameObject);
